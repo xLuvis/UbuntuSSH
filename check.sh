6db9aee476bf4615d1175e09f03cc5ef69b41ece
K1NG <77781079+K1NG-Rv@users.noreply.github.com>
2021-07-08 15:12:40 +0700

    Initial commit

---
+++ check.sh
@@ -0,0 +1,6 @@
+echo ubuntuSSH are xyzprjkt property
+git clone https://github.com/kdrag0n/proton-clang proton --depth=1
+git clone https://github.com/mose-to/xKernelCompiler compiler
+git clone https://github.com/mvaisakh/gcc-arm --depth=1
+git clone https://github.com/mvaisakh/gcc-arm64 --depth=1
+git clone https://github.com/mose-to/general-clang general --depth=1
